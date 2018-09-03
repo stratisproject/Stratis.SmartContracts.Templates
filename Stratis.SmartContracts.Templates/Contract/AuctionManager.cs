7e7bd9f390beb1faab3aa5a23b6d00bb6806ad48
Rowand de Haas <rowan.dehaas@stratisplatform.com>
2018-09-03 16:51:10 +1000

    Add contract creation support

---
+++ Stratis.SmartContracts.Templates/Contract/AuctionManager.cs
@@ -0,0 +1,16 @@
+﻿using Stratis.SmartContracts;
+
+public class AuctionManager : SmartContract
+{
+    public AuctionManager(ISmartContractState smartContractState)
+        : base(smartContractState)
+    {
+    }
+
+    public Address CreateNewAuction(ulong durationBlocks)
+    {
+        var createResult = this.Create<Auction>(new object[] { durationBlocks });
+
+        return createResult.NewContractAddress;
+    }
+}
