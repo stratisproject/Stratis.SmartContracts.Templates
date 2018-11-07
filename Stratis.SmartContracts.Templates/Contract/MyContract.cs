a8c2bc102b45738d5b8058dd13ca20e4183d1250
Jordan Andrews <jordandrews@hotmail.com>
2018-11-07 11:31:30 +1100

    Only the most basic contract

---
+++ Stratis.SmartContracts.Templates/Contract/MyContract.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Stratis.SmartContracts;
+
+public class MyContract : SmartContract
+{
+    public MyContract(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+    }
+}
